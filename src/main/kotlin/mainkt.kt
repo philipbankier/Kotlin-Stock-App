9bf13bd009f7bead932b2b3289c7762cf20a6380
philip bankier <philipbankier@gmail.com>
2017-08-29 21:25:39 -0400

    Added a timer

--- src/main/kotlin/mainkt.kt
+++ src/main/kotlin/mainkt.kt
@@ -1,15 +1,33 @@
 import yahoofinance.Stock
 import yahoofinance.YahooFinance
 import java.math.BigDecimal
+import java.util.Date
+import java.util.TimerTask
+import java.util.Timer
+
+
+fun timerRepeater() {
+    val repeatedTask = object : TimerTask() {
+        override fun run() {
+            var stock : Stock = YahooFinance.get("NVDA")
+            var price : BigDecimal = stock.getQuote().getPrice()
+            var change : BigDecimal = stock.getQuote().changeInPercent
+            stock.print()
+            print("Current Stock Price: ")
+            print(price)
+            print("\n\n Percent Change: ")
+            print(change)
+            print("\n\n")
+            println("Task performed on " + Date())
+        }
+    }
+    val timer = Timer("Timer")
+
+    val delay = 10000L
+    val period = 10000L
+    timer.scheduleAtFixedRate(repeatedTask, delay, period)
+}
 
 fun main(args: Array<String>) {
-    var stock : Stock = YahooFinance.get("NVDA")
-    var price : BigDecimal = stock.getQuote().getPrice()
-    var change : BigDecimal = stock.getQuote().changeInPercent
-    stock.print()
-    print("Current Stock Price: ")
-    print(price)
-    print("\n\n Percent Change: ")
-    print(change)
-    print("\n\n")
+    timerRepeater()
 }
